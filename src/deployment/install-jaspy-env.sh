2a70d89539fcd6b4212ea1e8cf6e8a532e271a4e
Ag Stephens <ag.stephens@stfc.ac.uk>
2018-10-15 18:55:32 +0000

    Updated scripts

--- src/deployment/install-jaspy-env.sh
+++ src/deployment/install-jaspy-env.sh
@@ -4,7 +4,7 @@ source ./set-defaults.sh
 
 env_name=$1
 
-ERR_MSG="ERROR: Please provide valid conda environment name as the first argument."
+ERR_MSG="[ERROR] Please provide valid conda environment name as the first argument."
 
 if [ ! $env_name ] ; then
     echo $ERR_MSG
@@ -12,21 +12,30 @@ if [ ! $env_name ] ; then
 fi
 
 channel_urls_fname="channel-urls.txt"
+pip_fname="pip.txt"
 
 spec_file_dir=$(find ../environments -name $env_name)
 spec_file_path=${spec_file_dir}/${channel_urls_fname}
+pip_file_path=${spec_file_dir}/${pip_fname}
 
 path_comps=$(echo $spec_file_dir | rev | cut -d/ -f2-3 | rev)
 bin_dir=${JASPY_BASE_DIR}/jaspy/miniconda_envs/jas${path_comps}/bin
 export PATH=${bin_dir}:$PATH
 
 cmd="${bin_dir}/conda create --name ${env_name} --file ${spec_file_path} -c ${JASPY_CHANNEL_URL}/jas${path_comps} --override-channels"
-echo "Running: $cmd"
+echo "[INFO] Running: $cmd"
 $cmd
 
-echo "Created conda environment: $env_name"
+echo "[INFO] Created conda environment: $env_name"
 
-echo "Activate it with:"
+if [ -f $pip_file_path ]; then
+    echo "[INFO] Installing additional packages via PIP..."
+    source ${bin_dir}/activate $env_name
+    ${bin_dir}/install pip
+    pip install -r ${pip_file_path} 
+fi
+
+echo "[INFO] You can activate and use this environment with:"
 echo "  export PATH=${bin_dir}:\$PATH"
 echo "  source activate $env_name"
 
