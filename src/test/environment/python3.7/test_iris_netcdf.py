d0183f052a1e17108b86a126a272675cfdad115c
Ag Stephens <ag.stephens@stfc.ac.uk>
2018-12-19 09:45:27 +0000

    Deleted old iris netcdf test - deprecated

--- src/test/environment/python3.7/test_iris_netcdf.py
+++
@@ -1,8 +0,0 @@
-
-def test_iris_cf_units_compatibility():
-    import cf_units
-    from iris.time import PartialDateTime as PDT
-    tu = cf_units.Unit('hours since 1970-01-01 0:0:0', calendar='360_day')
-    pdt1 = PDT(1970, 2, 1, 0, 0, 0)
-    tu.date2num(pdt1)
-
