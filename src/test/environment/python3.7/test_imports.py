da3744c9b7d6ea5c2b61ab3ae5c99c970be0ae93
Ag Stephens <ag.stephens@stfc.ac.uk>
2018-12-19 09:26:39 +0000

    Added new tests at python3.7

---
+++ src/test/environment/python3.7/test_imports.py
@@ -0,0 +1,12 @@
+
+def test_import_iris():
+    import iris
+    assert (iris.__version__.count(".") == 2)
+
+def test_import_netCDF4():
+    import netCDF4
+    assert (netCDF4.__version__.count(".") == 2)
+
+def test_import_cftime():
+    import cftime
+
